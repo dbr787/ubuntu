db62de0a41fa4432b17424f076c45de0065f828f
Daniel Ring <dbr787@gmail.com>
2020-06-03 14:41:28 +1000

    add vagrant examples

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,2 @@
+echo "-----> starting bootstrap.sh script"
+# sudo xrandr -s 1680x1050
---
+++ vagrant/prepare.sh
@@ -0,0 +1,22 @@
+echo "-----> starting prepare.sh script"
+
+# echo "-----> update and upgrade"
+# sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
+# echo "apt-get install -y ubuntu-desktop"
+# apt-get install -y ubuntu-desktop
+# echo "-----> installing build-essential"
+# apt-get install -y build-essential
+# echo "-----> installing dkms"
+# apt-get install -y dkms
+# echo "-----> installing linux-headers-$(uname -r)"
+# apt-get install -y linux-headers-$(uname -r)
+# echo "-----> installing virtualbox-guest-dkms "
+# apt-get install -y virtualbox-guest-dkms
+# echo "-----> installing virtualbox-guest-utils "
+# apt-get install -y virtualbox-guest-utils
+# echo "installing virtualbox-guest-x11"
+# apt-get install -y virtualbox-guest-x11
+# echo "installing virtualbox-guest-additions-iso"
+# apt-get install -y virtualbox-guest-additions-iso
+# xrandr -s 1680x1050
+# shutdown -r now
